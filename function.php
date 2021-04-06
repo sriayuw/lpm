f4f15029149b93449c3833a1c4bcf29591dd2a50
sriayuw <ayuwandira3917@gmail.com>
2021-04-06 12:13:25 +0700

    change database

--- function.php
+++ function.php
@@ -4,10 +4,10 @@
 // $user = "root";
 // $pass = "";
 // $name = "lpm";
-$host = "sql110.epizy.com";
-$user = "epiz_28324066";
-$pass = "tL8CrKvjhH02xI";
-$name = "epiz_28324066_laporanpengaduanmasyarakat";
+$host = "us-cdbr-east-03.cleardb.com";
+$user = "b6f6ed8f159d8e";
+$pass = "df63285b";
+$name = "heroku_0bfd57e4782b20c";
 
 $conn = mysqli_connect($host, $user, $pass, $name) or die("Koneksi ke database gagal");
 mysqli_select_db($conn, $name) or die('Database is not found!');
--- koneksi.php
+++ koneksi.php
@@ -3,12 +3,17 @@
 // $user = "root";
 // $pass = "";
 // $name = "lpm";
-$host = "sql110.epizy.com";
-$user = "epiz_28324066";
-$pass = "tL8CrKvjhH02xI";
-$name = "epiz_28324066_laporanpengaduanmasyarakat";
+// $host = "sql110.epizy.com";
+// $user = "epiz_28324066";
+// $pass = "tL8CrKvjhH02xI";
+// $name = "epiz_28324066_laporanpengaduanmasyarakat";
+$host = "us-cdbr-east-03.cleardb.com";
+$user = "b6f6ed8f159d8e";
+$pass = "df63285b";
+$name = "heroku_0bfd57e4782b20c";
 
-$koneksi = mysqli_connect("localhost","root","","pengaduan_masyarakat");
+
+$koneksi = mysqli_connect($host, $user, $pass, $name);
 $conn = mysqli_connect($host, $user, $pass, $name) or die("Koneksi ke database gagal");
 mysqli_select_db($conn, $name) or die('Database is not found!');
 // Check connection
